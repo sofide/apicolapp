3a6b2dfe860146fc7a08c046bc9fa3ae466e7b79
Sofia Denner <sofi.denner@gmail.com>
2018-05-29 13:11:59 -0300

    added label in apiary

--- apiary/models.py
+++ apiary/models.py
@@ -5,7 +5,8 @@ class Apiary(models.Model):
     '''location where beehives and nucs are kept'''
     hive = models.IntegerField(default=0)
     nuc = models.IntegerField(default=0)
+    label = models.CharField(max_length=200)
     owner = models.ForeignKey('auth.User', related_name='apiaries', on_delete=models.CASCADE)
 
     def __str__(self):
-        return '{} - {} nuc - {} hive'
+        return "{} - {} - {} nuc - {} hive".format(self.label, self.owner, self.nuc, self.hive)
