17403efbc8ef0b89594924418f09d8376835d625
Sofia Denner <sofi.denner@gmail.com>
2018-05-30 20:19:03 -0300

    added verbose_name

--- apiary/models.py
+++ apiary/models.py
@@ -3,9 +3,9 @@ from django.db import models
 
 class Apiary(models.Model):
     '''location where beehives and nucs are kept'''
-    hive = models.IntegerField(default=0)
-    nuc = models.IntegerField(default=0)
-    label = models.CharField(max_length=200)
+    hive = models.IntegerField(default=0, verbose_name='Colmenas')
+    nuc = models.IntegerField(default=0, verbose_name='Núcleos')
+    label = models.CharField(max_length=200, verbose_name='Nombre')
     owner = models.ForeignKey('auth.User', related_name='apiaries', on_delete=models.CASCADE)
 
     def __str__(self):
