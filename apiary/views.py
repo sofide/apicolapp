1bd6b7a030ee8f74b12a34f11c66105e8499406e
Sofia Denner <sofi.denner@gmail.com>
2018-05-30 17:16:47 -0300

    apiary index

--- apiary/views.py
+++ apiary/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
+from apiary.models import Apiary
+
 
 def index(request):
-    pass
+    if request.user.is_authenticated:
+        apiaries = Apiary.objects.filter(owner=request.user)
+
+    else:
+        apiaries = []
+
+    return render(request, 'apiary/index.html', {'apiaries': apiaries})
