252903c8ca58779c0bf67823edcda95b02b8fbdd
Sofia Denner <sofi.denner@gmail.com>
2018-06-12 16:46:11 -0300

    refactor apiary models

--- apiary/models.py
+++ apiary/models.py
@@ -1,34 +1,52 @@
+import datetime
+
 from django.db import models
 
 
 class Apiary(models.Model):
-    '''location where beehives and nucs are kept'''
+    """
+    Location where beehives and nucs are kept.
+    """
     label = models.CharField(max_length=200, verbose_name='Nombre')
     owner = models.ForeignKey('auth.User', related_name='apiaries', on_delete=models.CASCADE)
+    status = models.ForeignKey('ApiaryStatus', on_delete=models.CASCADE, related_name='current')
+
+    class Meta:
+        ordering = ['owner', 'label']
 
     def __str__(self):
         return self.label
 
+    def save(self, *args, **kwars):
+        """
+        If there is not status asociated, it creates an ApiaryStatus and add it to self.status.
+        """
+        if not ApiaryStatus.objects.filter(apiary=self).exists():
+            initial_status = ApiaryStatus(
+                apiary=self,
+                date=datetime.date.today(),
+                nucs=0,
+                hives=0
+            )
+            initial_status.save()
+            self.status = initial_status
+
+        if not self.status:
+            self.status = ApiaryStatus.objects.filter(apiary=self).first()
+
+
+
+        super(Apiary, self).save(*args, **kwars)
+
 
 class ApiaryStatus(models.Model):
-    apiary = models.ForeignKey(Apiary, on_delete=models.CASCADE, related_name='status')
+    apiary = models.ForeignKey(Apiary, on_delete=models.CASCADE, related_name='status_history')
     date = models.DateField(verbose_name='fecha')
     nucs = models.IntegerField(verbose_name='núcleos')
     hives = models.IntegerField(verbose_name='colmenas')
-    current = models.BooleanField(default=False)
 
-    def save(self, *args, **kwargs):
-        '''it shouldn't save two current status for the same apiary'''
-        if self.current:
-            try:
-                old_current = ApiaryStatus.objects.get(apiary=self.apiary, current=True)
-                if self != old_current:
-                    old_current.current = False
-                    old_current.save()
-            except ApiaryStatus.DoesNotExist:
-                pass
-
-        super(ApiaryStatus, self).save(*args, **kwargs)
+    class Meta:
+        ordering = ['-date']
 
 
 class Harvest(models.Model):
