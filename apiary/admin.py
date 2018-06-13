7403158647c57a9517f7c735bb772eb65b094708
Sofia Denner <sofi.denner@gmail.com>
2018-06-13 15:00:17 -0300

    added new apiary models to admin

--- apiary/admin.py
+++ apiary/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from apiary.models import Apiary
+from apiary.models import Apiary, ApiaryStatus, Harvest
 
 
 admin.site.register(Apiary)
+admin.site.register(ApiaryStatus)
+admin.site.register(Harvest)
