cf466852de8c8ac9078f40e28711622422783442
Sofia Denner <sofi.denner@gmail.com>
2018-06-13 16:19:40 -0300

    better view names

--- apiary/urls.py
+++ apiary/urls.py
@@ -5,6 +5,6 @@ from apiary import views
 
 urlpatterns = [
     path('', views.index, name='apiary_index'),
-    path('new/', views.edit_apiary, name='new_apiary'),
-    path('edit/<int:apiary_pk>', views.edit_apiary, name='edit_apiary'),
+    path('new/', views.edit_apiary, name='apiary_new'),
+    path('edit/<int:apiary_pk>', views.edit_apiary, name='apiary_edit'),
 ]
