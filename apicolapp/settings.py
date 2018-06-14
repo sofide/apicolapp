be29fb70df794bdc0c306d51e8f46b5220143831
Sofia Denner <sofi.denner@gmail.com>
2018-06-14 12:05:35 -0300

    date formats updated

--- apicolapp/settings.py
+++ apicolapp/settings.py
@@ -115,9 +115,16 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_L10N = True
+USE_L10N = False
 
 USE_TZ = True
+
+DATE_FORMAT = 'd/m/Y'
+
+DATE_INPUT_FORMATS = [
+    '%d/%m/%Y',
+    '%d/%m/%y',
+]
 
 
 # Static files (CSS, JavaScript, Images)
