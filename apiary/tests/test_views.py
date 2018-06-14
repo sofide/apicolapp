44c01db215989d2fe3887d0bad26e499b077a162
Sofia Denner <sofi.denner@gmail.com>
2018-06-14 10:49:39 -0300

    tests documented

--- apiary/tests/test_views.py
+++ apiary/tests/test_views.py
@@ -15,7 +15,7 @@ def test_apiary_index_is_working(client, django_user_model):
 
 def test_manolo_has_apiaries(client_logged_as_manolo, the_twenty_apiaries_of_manolo):
     """
-    Test if manolo's apiaries are sent to template
+    Test if manolo's apiaries are sent to template.
     """
     response = client_logged_as_manolo.get(reverse('apiary_index'))
 
@@ -26,12 +26,18 @@ def test_manolo_has_apiaries(client_logged_as_manolo, the_twenty_apiaries_of_man
 
 
 def test_new_apiary_is_working(client_logged_as_manolo):
+    """
+    Test if new apiary view es working.
+    """
     response = client_logged_as_manolo.get(reverse('apiary_new'))
 
     assert response.status_code == 200
 
 
 def test_new_apiary_is_redirecting_if_user_isnt_logged(client):
+    """
+    Test if new_apiary redirects if user is not logged.
+    """
     response = client.get(reverse('apiary_new'))
 
     assert response.status_code == 302
@@ -39,12 +45,18 @@ def test_new_apiary_is_redirecting_if_user_isnt_logged(client):
 
 
 def test_edit_apiary_is_working(client_logged_as_manolo, apiary_of_manolo):
+    """
+    Test if edit user's apiary view is working.
+    """
     response = client_logged_as_manolo.get(reverse('apiary_edit', args=(apiary_of_manolo.pk,)))
 
     assert response.status_code == 200
 
 
 def test_edit_apiary_of_other_user_redirect_you(client_logged_as_manolo, apiary_of_samuel):
+    """
+    Test if edit another user's apiary view is redirecting to apiary index.
+    """
     response = client_logged_as_manolo.get(reverse('apiary_edit', args=(apiary_of_samuel.pk,)))
 
     assert response.status_code == 302
