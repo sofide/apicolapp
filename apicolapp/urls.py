6e2bb448469ce93c6eabafd8bdbb6f8832a3a1aa
Sofia Denner <sofi.denner@gmail.com>
2018-08-04 17:32:32 -0300

    add signup

--- apicolapp/urls.py
+++ apicolapp/urls.py
@@ -7,6 +7,7 @@ from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/signup/', views.signup, name='signup'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', views.home, name='home'),
     path('apiary/', include('apiary.urls')),
--- core/views.py
+++ core/views.py
@@ -1,5 +1,23 @@
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect
+
+from core.forms import SignUpForm
 
 
 def home(request):
     return render(request, 'core/home.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'registration/signup.html', {'form': form})
