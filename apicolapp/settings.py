65eccc8475b1519e2a5cafb66db0dfea97c8e651
Sofia Denner <sofi.denner@gmail.com>
2018-08-04 19:54:47 -0300

    delete commented app

--- apicolapp/settings.py
+++ apicolapp/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'livereload',
-    # 'core',
     'user_manager',
     'apiary.app.ApiaryConfig',
     'accounting',
