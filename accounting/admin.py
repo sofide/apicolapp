941eae982102831401cf9026d24767ee014ea4a9
Sofia Denner <sofi.denner@gmail.com>
2019-01-15 20:50:32 -0300

    add data in Purchase admin

--- accounting/admin.py
+++ accounting/admin.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 
 from accounting import models
 
+class PurchaseAdmin(admin.ModelAdmin):
+    list_filter = ('product', 'amount', 'value')
+    list_display = ('__str__', 'product', 'amount', 'value')
 
 admin.site.register(models.Product)
-admin.site.register(models.Purchase)
+admin.site.register(models.Purchase, PurchaseAdmin)
 admin.site.register(models.Category)
 admin.site.register(models.DepreciationInfo)
